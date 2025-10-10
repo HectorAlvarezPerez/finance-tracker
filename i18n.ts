9e5809edf31329f7195262462e9c8dfa5ad1ce65
HectorAlvarezPerez <hector_alvarez_perez@outlook.es>
2025-10-10 22:50:20 +0200

    fix: configure next-intl for server components
    - Create i18n.ts config file in root (required by next-intl)
    - Update next.config.js to use next-intl plugin
    - Remove unused lib/i18n/config.ts
    - Fixes 'Couldn't find next-intl config file' error
    - getTranslations now works in server components

---
+++ i18n.ts
@@ -0,0 +1,12 @@
+import { getRequestConfig } from 'next-intl/server'
+
+export default getRequestConfig(async () => {
+  // Default to English - the I18nProvider will handle dynamic switching on client
+  const locale = 'en'
+
+  return {
+    locale,
+    messages: (await import(`./messages/${locale}.json`)).default
+  }
+})
+
--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const withNextIntl = require('next-intl/plugin')()
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -7,5 +9,5 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+module.exports = withNextIntl(nextConfig)
 
