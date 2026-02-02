703b8538730b2d37529e3dcd2976a3d1931b28f5
HectorAlvarezPerez <hector_alvarez_perez@outlook.es>
2026-02-02 18:38:23 +0100

    build: Create OpenNext deployment output for the finance tracker application.

---
+++ .open-next/server-functions/default/.next/server/webpack-runtime.js
@@ -0,0 +1,29 @@
+(()=>{"use strict";var e={},r={};function t(o){var n=r[o];if(void 0!==n)return n.exports;var a=r[o]={id:o,loaded:!1,exports:{}},d=!0;try{e[o].call(a.exports,a,a.exports,t),d=!1}finally{d&&delete r[o]}return a.loaded=!0,a.exports}t.m=e,t.amdO={},t.n=e=>{var r=e&&e.__esModule?()=>e.default:()=>e;return t.d(r,{a:r}),r},(()=>{var e,r=Object.getPrototypeOf?e=>Object.getPrototypeOf(e):e=>e.__proto__;t.t=function(o,n){if(1&n&&(o=this(o)),8&n||"object"==typeof o&&o&&(4&n&&o.__esModule||16&n&&"function"==typeof o.then))return o;var a=Object.create(null);t.r(a);var d={};e=e||[null,r({}),r([]),r(r)];for(var l=2&n&&o;"object"==typeof l&&!~e.indexOf(l);l=r(l))Object.getOwnPropertyNames(l).forEach(e=>d[e]=()=>o[e]);return d.default=()=>o,t.d(a,d),a}})(),t.d=(e,r)=>{for(var o in r)t.o(r,o)&&!t.o(e,o)&&Object.defineProperty(e,o,{enumerable:!0,get:r[o]})},t.f={},t.e=e=>Promise.all(Object.keys(t.f).reduce((r,o)=>(t.f[o](e,r),r),[])),t.u=e=>""+e+".js",t.o=(e,r)=>Object.prototype.hasOwnProperty.call(e,r),t.r=e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},t.nmd=e=>(e.paths=[],e.children||(e.children=[]),e),t.X=(e,r,o)=>{var n=r;o||(r=e,o=()=>t(t.s=n)),r.map(t.e,t);var a=o();return void 0===a?e:a},t.nc=void 0,(()=>{var e={658:1},r=r=>{var o=r.modules,n=r.ids,a=r.runtime;for(var d in o)t.o(o,d)&&(t.m[d]=o[d]);a&&a(t);for(var l=0;l<n.length;l++)e[n[l]]=1};t.f.require=(o, _) => {
+  if (!e[o]) {
+    switch (o) {
+       case 147: r(require("./chunks/147.js")); break;
+       case 161: r(require("./chunks/161.js")); break;
+       case 286: r(require("./chunks/286.js")); break;
+       case 316: r(require("./chunks/316.js")); break;
+       case 41: r(require("./chunks/41.js")); break;
+       case 421: r(require("./chunks/421.js")); break;
+       case 487: r(require("./chunks/487.js")); break;
+       case 506: r(require("./chunks/506.js")); break;
+       case 631: r(require("./chunks/631.js")); break;
+       case 671: r(require("./chunks/671.js")); break;
+       case 746: r(require("./chunks/746.js")); break;
+       case 786: r(require("./chunks/786.js")); break;
+       case 787: r(require("./chunks/787.js")); break;
+       case 818: r(require("./chunks/818.js")); break;
+       case 833: r(require("./chunks/833.js")); break;
+       case 843: r(require("./chunks/843.js")); break;
+       case 864: r(require("./chunks/864.js")); break;
+       case 865: r(require("./chunks/865.js")); break;
+       case 902: r(require("./chunks/902.js")); break;
+       case 935: r(require("./chunks/935.js")); break;
+       case 658: e[o] = 1; break;
+       default: throw new Error(`Unknown chunk ${o}`);
+    }
+  }
+}
+,module.exports=t,t.C=r})()})();
